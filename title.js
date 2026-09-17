a66039e14d97ee5cff3aebf82011b122c39f8714
J Schere <owner@jschere.com>
2026-09-17 00:06:57 -0400

    big changes to title page, almost done

--- title.js
+++ title.js
@@ -2,3 +2,24 @@
 // hack club rocks
 
 console.log("reborn at last")
+
+let mouseFollower = document.createElement("div");
+document.body.appendChild(mouseFollower);
+mouseFollower.id = "follower";
+document.addEventListener("mousemove", (ev) => {
+    mouseFollower.style.top = ev.clientY + "px";
+    mouseFollower.style.left = ev.clientX + "px";
+})
+
+// messing around with a generator function:
+function* ellipsis() {
+    while (true) {
+        yield ".";
+        yield "..";
+        yield "...";
+    }
+}
+let gen = ellipsis();
+setInterval(() => {
+    document.querySelector("#enter p").innerText = "enter" + gen.next().value;
+},1000)
